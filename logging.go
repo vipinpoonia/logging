97a27eab74d994626628f1983d437931b06393f8
Vipin Poonia <vipinkpoonia05@gmail.com>
2019-08-31 05:16:44 +0530

    added init in logging

--- logging.go
+++ logging.go
@@ -3,20 +3,26 @@ package logging
 import (
 	log "github.com/sirupsen/logrus"
 	"os"
+	"sync"
 )
 
 const timeStampFormat = "2006-01-02 15:04:05.999999999Z07:00"
 
-func Init(conf LogConfig) error {
-	log.SetFormatter(&log.TextFormatter{
-		DisableColors:   false,
-		FullTimestamp:   true,
-		TimestampFormat: timeStampFormat,
+func init() {
+	var once sync.Once
+	once.Do(func() {
+		log.SetFormatter(&log.TextFormatter{
+			DisableColors:   false,
+			FullTimestamp:   true,
+			TimestampFormat: timeStampFormat,
+		})
+		log.SetLevel(log.TraceLevel)
+		log.SetOutput(os.Stdout)
+		log.Info("logger is configured")
 	})
-	log.SetLevel(log.TraceLevel)
-	log.SetOutput(os.Stdout)
-	log.Info("logger is configured")
+}
 
+func Init(conf LogConfig) error {
 	if conf.SentryDsn == "" {
 		log.Warn("SentryDsn is missing")
 	} else {
